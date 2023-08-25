1294e696bfb186d37fb97b69e390a6f3450ab5e0
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-25 11:00:01 +0200

    Adding only cors()

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,17 @@
 // import some node modules for later
 const path = require("node:path");
+const cors = require("cors");
 
 // create express app
 
 const express = require("express");
-const { corsMiddleware } = require("./middlewares/cors");
+// const { corsMiddleware } = require("./middlewares/cors");
 
 const app = express();
 
 // use some application-level middlewares
-app.use(corsMiddleware);
+// app.use(corsMiddleware);
+app.use(cors());
 
 app.use(express.json());
 
