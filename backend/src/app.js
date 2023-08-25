ce57c83071d8ea877e49c814a48d07874edbc566
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-25 10:47:58 +0200

    Putting back cors in back

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,12 @@ const path = require("node:path");
 // create express app
 
 const express = require("express");
-// const { corsMiddleware } = require("./middlewares/cors");
+const { corsMiddleware } = require("./middlewares/cors");
 
 const app = express();
 
 // use some application-level middlewares
-// app.use(corsMiddleware);
+app.use(corsMiddleware);
 
 app.use(express.json());
 
